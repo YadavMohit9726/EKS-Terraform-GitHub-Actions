2b37b1bf0386bfbb94fa7723312bf2bba45b679f
Mohit Yadav <164593784+YadavMohit9726@users.noreply.github.com>
2025-05-04 20:47:09 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,19 +1,4 @@
-terraform {
-  required_version = "~> 1.11.4"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
-    }
-  }
-  backend "s3" {
-    bucket         = "dev-mohit-tf-bucket"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "dev-mohit-tf-table"
-    encrypt        = true
-  }
-}
+
 
 provider "aws" {
   region  = var.aws-region
